72a132a515c5d73d07d929509fc97b20793c8c14
vannucci <homelete@gmail.com>
2022-01-10 18:05:34 -0300

    incluído outro contador

--- assets/js/script_eventListener.js
+++ assets/js/script_eventListener.js
@@ -1,15 +1,57 @@
 var number = document.getElementById('currentNumber')
-var somar = document.getElementById('somar')
-var subtrair = document.getElementById('subtrair')
+var anothernumber = document.getElementById('anotherNumber')
+
+var somar = document.getElementById('mais')
+var subtrair = document.getElementById('menos')
 
 var currentNumber = 0 ;
 
 function increment(){
     currentNumber = currentNumber+1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    number.innerHTML = currentNumber;
 }
 
 function decrement(){
     currentNumber = currentNumber-1;
-    currentNumberWrapper.innerHTML = currentNumber;
+    number.innerHTML = currentNumber;
 }
+
+somar.addEventListener('click', function(){
+    if (currentNumber>=-1){
+        currentNumber = currentNumber+1
+        anothernumber.innerHTML=currentNumber
+        anothernumber.style.color='black'
+    }else{
+        currentNumber=currentNumber+1
+        anothernumber.innerHTML = currentNumber}
+})
+
+/*
+somar.addEventListener('click', function(){
+    if(currentNumber>=10){
+        currentNumber = currentNumber
+    }else{
+    currentNumber=currentNumber+1
+    anothernumber.innerHTML = currentNumber}
+})
+
+
+subtrair.addEventListener('click', function(){
+    if (currentNumber<=0){
+        currentNumber = currentNumber
+    }else{
+        currentNumber=currentNumber-1
+        anothernumber.innerHTML = currentNumber}
+    
+})*/
+
+subtrair.addEventListener('click', function(){
+    if (currentNumber<=0){
+        currentNumber = currentNumber-1
+        anothernumber.innerHTML=currentNumber
+        anothernumber.style.color='red'
+    }else{
+        currentNumber=currentNumber-1
+        anothernumber.innerHTML = currentNumber}
+    
+})
